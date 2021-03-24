d2cca85690bf724e8b2656bfd6f13e8a2da5673b
EvgeniiaJ <78631584+EvgeniiaJ@users.noreply.github.com>
2021-03-23 20:45:42 -0400

    Update IntegrationLauncher.java

--- IntegrationLauncher.java
+++ IntegrationLauncher.java
@@ -10,15 +10,19 @@ public class IntegrationLauncher {
 			IntegrationMethods integration = new IntegrationMethods();
 
 			double x0 = 0;
+			
+			// choose one of the following two values y0 or add another one
 //			double y0 = 1;
-			double y0 = (Math.sqrt(5) - 1) / 2d;
-			double h = 0.001d;
-			double x = 10d;
+			double y0 = (Math.sqrt(5) - 1) / 2.0d;
+			
+			double h = 0.001d; // the size of the step 
+			double x = 10.0d;  // final value the stop of approximation
 
 			integration.eulerMethod(x0, y0, h, x, outputFile);
 			integration.midpointRule(x0, y0, h, x, outputFile);
 			integration.trapezoidRule(x0, y0, h, x, outputFile);
 			integration.rungeKutta(x0, y0, h, x, outputFile);
+			
 			outputFile.close();
 
 		} catch (Exception e) {
